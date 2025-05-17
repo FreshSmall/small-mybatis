e65467a90a49c0dc45c5c2768dd9412643eae4e5
yinchao <ycsuper2819@gmail.com>
2025-05-17 22:45:46 +0800

    feat：优化数据库池化的属性填写

--- src/main/java/com/demo/mybatis/datasource/pooled/PooledDataSourceFactory.java
+++ src/main/java/com/demo/mybatis/datasource/pooled/PooledDataSourceFactory.java
@@ -1,7 +1,5 @@
 package com.demo.mybatis.datasource.pooled;
 
-import javax.sql.DataSource;
-
 import com.demo.mybatis.datasource.unpooled.UnpooledDataSourceFactory;
 
 /**
@@ -13,13 +11,7 @@ import com.demo.mybatis.datasource.unpooled.UnpooledDataSourceFactory;
  */
 public class PooledDataSourceFactory extends UnpooledDataSourceFactory {
 
-    @Override
-    public DataSource getDataSource() {
-        PooledDataSource pooledDataSource = new PooledDataSource();
-        pooledDataSource.setDriver(properties.getProperty("driver"));
-        pooledDataSource.setUrl(properties.getProperty("url"));
-        pooledDataSource.setUsername(properties.getProperty("username"));
-        pooledDataSource.setPassword(properties.getProperty("password"));
-        return pooledDataSource;
+    public PooledDataSourceFactory() {
+        this.dataSource = new PooledDataSource();
     }
 }
