981048c51457dd690137d81397a14b56fc662240
yinchao <ycsuper2819@gmail.com>
2025-05-22 18:08:58 +0800

    feat：初始化解析动态 sql语句的需求

--- src/main/java/com/demo/mybatis/scripting/xmltags/XMLScriptBuilder.java
+++ src/main/java/com/demo/mybatis/scripting/xmltags/XMLScriptBuilder.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.dom4j.Element;
+import org.dom4j.Node;
 
 import com.demo.mybatis.builder.BaseBuilder;
 import com.demo.mybatis.mapping.SqlSource;
@@ -15,7 +16,7 @@ import com.demo.mybatis.session.Configuration;
  * @Date: 2025-05-19 22:50:36
  * @LastEditors: yinchao
  * @LastEditTime: 2025-05-19 22:50:37
- * @Description: 
+ * @Description:
  */
 public class XMLScriptBuilder extends BaseBuilder {
 
@@ -32,14 +33,52 @@ public class XMLScriptBuilder extends BaseBuilder {
     public SqlSource parseScriptNode() {
         List<SqlNode> contents = parseDynamicTags(element);
         MixedSqlNode rootSqlNode = new MixedSqlNode(contents);
-        return new RawSqlSource(configuration, rootSqlNode, parameterType);
+
+        // 判断是否为动态SQL
+        if (isDynamic) {
+            return new DynamicSqlSource(configuration, rootSqlNode);
+        } else {
+            return new RawSqlSource(configuration, rootSqlNode, parameterType);
+        }
     }
 
     List<SqlNode> parseDynamicTags(Element element) {
         List<SqlNode> contents = new ArrayList<>();
-        // element.getText 拿到 SQL
-        String data = element.getText();
-        contents.add(new StaticTextSqlNode(data));
+
+        // 处理元素的文本内容和子元素
+        List<Node> children = element.content();
+        for (Node child : children) {
+            if (child.getNodeType() == Node.TEXT_NODE) {
+                // 处理文本节点
+                String text = child.getText().trim();
+                if (text.length() > 0) {
+                    TextSqlNode textSqlNode = new TextSqlNode(text);
+                    if (textSqlNode.isDynamic()) {
+                        contents.add(textSqlNode);
+                        isDynamic = true;
+                    } else {
+                        contents.add(new StaticTextSqlNode(text));
+                    }
+                }
+            } else if (child.getNodeType() == Node.ELEMENT_NODE) {
+                // 处理元素节点
+                Element childElement = (Element) child;
+                String nodeName = childElement.getName();
+
+                // 处理if标签
+                if ("if".equals(nodeName)) {
+                    String test = childElement.attributeValue("test");
+                    List<SqlNode> ifContents = parseDynamicTags(childElement);
+                    MixedSqlNode mixedSqlNode = new MixedSqlNode(ifContents);
+                    contents.add(new IfSqlNode(mixedSqlNode, test));
+                    isDynamic = true;
+                } else {
+                    // 处理其他标签类型（未来可扩展）
+                    // 目前暂不支持其他标签
+                }
+            }
+        }
+
         return contents;
     }
 }
